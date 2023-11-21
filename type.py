cbd4d5b89f0b02eb3dec6876017de1da795e84c3
ali fathi <ali.fathi.13121110@gmail.com>
2023-11-21 20:39:27 +0330

    add try/except

--- type.py
+++ type.py
@@ -9,17 +9,17 @@ driver = webdriver.Firefox(options=options)
 driver.get('https://typetest.io/')
 
 
-driver.implicitly_wait(4)
+driver.implicitly_wait(2)
 while True:
-    driver.implicitly_wait(3)
-    chars = driver.find_elements(By.CLASS_NAME, 'test-char')
+    try:
+        chars = driver.find_elements(By.CLASS_NAME, 'test-char')
 
-    for char in chars:
-        print(char.text, end='')
+        input_field = driver.find_element(By.ID, 'test-input')
 
-    input_field = driver.find_element(By.ID, 'test-input')
-
-    for char in chars:
-        input_field.send_keys(char.text)
+        for char in chars:
+            input_field.send_keys(char.text)
+            
+    except:
+        print("ERROR!")
 
 
