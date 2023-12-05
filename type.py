234864a8f06f5edce3497bc4d1993a7826d3f10f
ali fathi <ali.fathi.13121110@gmail.com>
2023-12-05 17:15:27 +0330

    feat: add login to site

--- type.py
+++ type.py
@@ -2,12 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
+
+email = input("your emai-> ")
+password = input("your password-> ")
+
 options = Options()
 options.headless = False
 driver = webdriver.Firefox(options=options)
 
-driver.get('https://typetest.io/')
+driver.get('https://typetest.io/login')
 
+driver.implicitly_wait(2)
+
+input_email = driver.find_element(By.ID, 'email')
+input_email.send_keys(email)
+
+input_password = driver.find_element(By.ID, 'password')
+input_password.send_keys(password)
+
+btn_login = driver.find_element(By.CLASS_NAME, 'button-highlight')
+btn_login.click()
 
 driver.implicitly_wait(2)
 while True:
